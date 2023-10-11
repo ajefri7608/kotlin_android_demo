5d8e5bb6eb052595586290ade7ff30f1cc6dd3c3
jefriSaeTem <ajefri7608@gmail.com>
2023-10-11 21:40:17 +0800

    feature: bottom tab shake animation

--- app/src/main/java/com/example/kotlinandroiddemo/view/bottomTab/BottomTabBar.kt
+++ app/src/main/java/com/example/kotlinandroiddemo/view/bottomTab/BottomTabBar.kt
@@ -1,14 +1,23 @@
 package com.example.kotlinandroiddemo.view.bottomTab
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.animate
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -19,6 +28,11 @@ import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.kotlinandroiddemo.model.Screen
 import com.example.kotlinandroiddemo.ui.theme.Typography
+import com.example.kotlinandroiddemo.utils.compose.shake
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+
+
 
 
 @Composable
@@ -35,16 +49,45 @@ fun bottomTabBar(navController: NavController) {
             Screen.LoginScreen,
             Screen.SettingScreen
         )
+        val scope = rememberCoroutineScope()
         val navBackStackEntry by navController.currentBackStackEntryAsState()
         val currentDestination = navBackStackEntry?.destination
+        val iconOffset = remember { Animatable(0f) }
         items.forEachIndexed { index, screen ->
             val selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true
             BottomNavigationItem(
-                icon = { BottomTabIcon(icon = screen.icon, selected) },
-                label = { Text(text = stringResource(screen.title), color = Color("#748c94".toColorInt()), fontSize = 12.sp) },
+                icon = {
+                    Image(
+                        painter = painterResource(id = screen.icon),
+                        modifier = Modifier
+                            .shake(selected)
+                            .size(21.dp),
+                        contentDescription = null,
+                        colorFilter = ColorFilter.tint(
+                            if (selected == true) Color("#00c090".toColorInt()) else Color("#748c94".toColorInt())
+                        )
+                    )
+                },
+                label = {
+                    Text(
+                        text = stringResource(screen.title),
+                        color = Color("#748c94".toColorInt()),
+                        fontSize = 12.sp
+                    )
+                },
                 selected = selected,
                 alwaysShowLabel = false,
                 onClick = {
+                    scope.launch {
+                        iconOffset.animateTo(
+                            20f,
+                            animationSpec = tween(2000),
+                        )
+                        iconOffset.animateTo(
+                            10f,
+                            animationSpec = tween(2000),
+                        )
+                    }
                     navController.navigate(screen.route) {
                         // Pop up to the start destination of the graph to
                         // avoid building up a large stack of destinations
